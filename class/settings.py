a419148387e2da20efd6c1050cb0b9673393aea5
papu-hacker <173533120+papu-hacker@users.noreply.github.com>
2025-12-26 04:08:33 +0000

    remove tailwind-css

--- class/settings.py
+++ class/settings.py
@@ -41,13 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'tools',
-    'tailwind',
-    'theme',
-    'django_browser_reload',
 ]
 
-TAILWIND_APP_NAME = 'theme'
-INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'class.urls'
